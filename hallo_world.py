40e1e123e3dd99bb4de8f1baa1da3b8c6e61651c
nhpntrythebest <nguyenhongphuongnghi2001@gmail.com>
2026-02-23 11:59:14 +0100

    Update hallo_world.py

--- hallo_world.py
+++ hallo_world.py
@@ -1,8 +1,8 @@
-# Bài tập đầu tiên: Chào thế giới và máy tính
-print("Chào bạn! Tôi là Nghi.")
-print("Tôi đang bắt đầu hành trình trở thành lập trình viên tại Đức.")
+# Test 1
+print("Hallo! Ich bin Nghi.")
+print("Ich werde eine Fachinfortechniker in Deutschland.")
 
-# Một phép tính đơn giản thể hiện tư duy logic
+# Test 2
 a = 5
 b = 10
-print("Tổng của 5 và 10 là:", a + b)
+print("Total value:", a + b)
