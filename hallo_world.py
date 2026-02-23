1f6e46792474974e1c32853d17881fc0a1df0be1
nhpntrythebest <nguyenhongphuongnghi2001@gmail.com>
2026-02-23 11:54:42 +0100

    Create hallo_world.py

---
+++ hallo_world.py
@@ -0,0 +1,8 @@
+# Bài tập đầu tiên: Chào thế giới và máy tính
+print("Chào bạn! Tôi là Nghi.")
+print("Tôi đang bắt đầu hành trình trở thành lập trình viên tại Đức.")
+
+# Một phép tính đơn giản thể hiện tư duy logic
+a = 5
+b = 10
+print("Tổng của 5 và 10 là:", a + b)
